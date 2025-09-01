203f7496e48c72b4374eaffe6ff965495a0fe896
mrabid <mdalabidsupto@gmail.com>
2025-09-02 00:51:39 +0600

    added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This line is crucial for relative asset paths in deployment
 })
